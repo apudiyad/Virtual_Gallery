f79d492365dc83b742ed90a537b26604bf0f4722
Shubham Bansal <Shubham99.sb@gmail.com>
2023-11-21 01:43:57 -0500

    changes regarding home and contact icon

--- assets/js/login.js
+++ assets/js/login.js
@@ -79,10 +79,19 @@ if (form) {
 
 const navBarLogin = document.getElementById("navbar-login");
 if (localStorage.getItem("auth") == 1) {
-  navBarLogin.innerHTML = "<li><a class=\"nav-link btn\" href=\"user.html\">User</a></li>" 
-  + "<li><a class=\"nav-link btn\" href=\"user.html#upcoming\">Events</a></li>"
-  + "<li><a class=\"nav-link btn\" href=\"user.html#savedart\">Saved Art</a></li>"
-  + "<li><a class=\"nav-link btn\" id=\"logoutBtn\">Logout</a><li>";
+  navBarLogin.innerHTML = `
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLoginContent" aria-controls="navbarLoginContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="bi bi-person-fill"></span> <!-- This is the user icon from Bootstrap Icons -->
+    </button>
+    <div class="collapse navbar-collapse" id="navbarLoginContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="user.html">User</a></li>
+            <li class="nav-item"><a class="nav-link" href="user.html#upcoming">Events</a></li>
+            <li class="nav-item"><a class="nav-link" href="user.html#savedart">Saved Art</a></li>
+            <li class="nav-item"><a class="nav-link" id="logoutBtn">Logout</a></li>
+        </ul>
+    </div>
+  `;
 } else {
   navBarLogin.innerHTML = "<li><a class=\"nav-link btn\" data-toggle=\"modal\" data-target=\"#login-modal\">Login</a></li>";
 }
@@ -101,3 +110,77 @@ if (logoutBtn != null) {
   }, false);
 }
 
+
+const navBarToggler = document.querySelector('.navbar-toggler'); // The user icon
+const navbarLoginContent = document.querySelector('#navbarLoginContent'); // The dropdown content
+
+let isHoveringOverIcon = false;
+let isHoveringOverDropdown = false;
+
+// Function to show the dropdown
+function showDropdown() {
+  $(navbarLoginContent).collapse('show');
+}
+
+// Function to hide the dropdown
+function hideDropdown() {
+  // Only hide if not hovering over icon or dropdown
+  if (!isHoveringOverIcon && !isHoveringOverDropdown) {
+    $(navbarLoginContent).collapse('hide');
+  }
+}
+
+// Mouse enter and leave for icon
+navBarToggler.addEventListener('mouseenter', function() {
+  isHoveringOverIcon = true;
+  showDropdown();
+});
+
+navBarToggler.addEventListener('mouseleave', function() {
+  isHoveringOverIcon = false;
+  setTimeout(hideDropdown, 500); // Gives a delay to allow moving to the dropdown
+});
+
+// Mouse enter and leave for dropdown content
+navbarLoginContent.addEventListener('mouseenter', function() {
+  isHoveringOverDropdown = true;
+});
+
+navbarLoginContent.addEventListener('mouseleave', function() {
+  isHoveringOverDropdown = false;
+  hideDropdown();
+});
+
+// CSS for nav user icon
+const style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = `
+  .bi-person-fill {
+    color: #fff;
+  }
+  .navbar-toggler {
+    background-color: transparent;
+    border: 2px solid #fff;
+    border-radius: 50%;
+    padding: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #navbarLoginContent {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    z-index: 1000;
+    background-color: rgba(255, 255, 255, 0);
+    width: 100%;
+  }
+  #navbarLoginContent .nav-link {
+    padding: 10px 15px;
+    color: #fff;
+  }
+  #navbarLoginContent .nav-item:hover .nav-link {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+`;
+document.getElementsByTagName('head')[0].appendChild(style);
